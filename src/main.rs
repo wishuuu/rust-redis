019ca2f7a333d38ed748da32965f473c72fa116f
Oskar Wiszowaty <ppwiszu@gmail.com>
2024-04-07 21:47:39 +0200

    Fix based on ai

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,20 @@
 use std::{
-    io::{Read, Write},
-    net::{TcpListener, TcpStream},
+    io::Write,
+    net::TcpListener,
 };
 
-fn handle_request(request: &str, stream: &mut TcpStream) -> Result<(), String> {
-    match request {
-        "PING" => {
-            let response = b"+PONG\r\n";
-            match stream.write_all(response) {
-                Ok(_) => Ok(()),
-                Err(e) => Err(e.to_string()),
-            }
-        }
-        _ => Err(format!("Do not support request {}", request)),
-    }
-}
+// fn handle_request(request: &str, stream: &mut TcpStream) -> Result<(), String> {
+//    match request {
+//        "PING" => {
+//            let response = b"+PONG\r\n";
+//            match stream.write_all(response) {
+//                Ok(_) => Ok(()),
+//                Err(e) => Err(e.to_string()),
+//            }
+//        }
+//        _ => Err(format!("Do not support request {}", request)),
+//    }
+//}
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -26,14 +26,16 @@ fn main() {
         match stream {
             Ok(mut _stream) => {
                 println!("accepted new connection");
-                let mut request = String::new();
-                _stream.read_to_string(&mut request).unwrap();
+                //let mut request = String::new();
+                //_stream.read_to_string(&mut request).unwrap();
 
-                match handle_request(&request, &mut _stream) {
-                    Ok(_) => (),
-                    Err(e) => println!("error: {}", e)
-                }
-
+                //match handle_request(&request, &mut _stream) {
+                //    Ok(_) => (),
+                //    Err(e) => println!("error: {}", e)
+                //}
+                
+                let response = b"+PONG\r\n";
+                _stream.write_all(response).unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
