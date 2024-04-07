40ba14683e17ef3aff9a818201bbd892d78c2b31
Oskar Wiszowaty <ppwiszu@gmail.com>
2024-04-07 21:54:47 +0200

    Split request by new lines

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,20 @@
 use std::{
-    io::Write,
-    net::TcpListener,
+    io::{Read, Write},
+    net::{TcpListener, TcpStream},
 };
 
-// fn handle_request(request: &str, stream: &mut TcpStream) -> Result<(), String> {
-//    match request {
-//        "PING" => {
-//            let response = b"+PONG\r\n";
-//            match stream.write_all(response) {
-//                Ok(_) => Ok(()),
-//                Err(e) => Err(e.to_string()),
-//            }
-//        }
-//        _ => Err(format!("Do not support request {}", request)),
-//    }
-//}
+fn handle_request(request: &str, stream: &mut TcpStream) -> Result<(), String> {
+    match request {
+        "ping" => {
+            let response = b"+PONG\r\n";
+            match stream.write_all(response) {
+                Ok(_) => Ok(()),
+                Err(e) => Err(e.to_string()),
+            }
+        }
+        _ => Err(format!("Do not support request {}", request)),
+    }
+}
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -26,16 +26,15 @@ fn main() {
         match stream {
             Ok(mut _stream) => {
                 println!("accepted new connection");
-                //let mut request = String::new();
-                //_stream.read_to_string(&mut request).unwrap();
+                let mut request = String::new();
+                _stream.read_to_string(&mut request).unwrap();
 
-                //match handle_request(&request, &mut _stream) {
-                //    Ok(_) => (),
-                //    Err(e) => println!("error: {}", e)
-                //}
-                
-                let response = b"+PONG\r\n";
-                _stream.write_all(response).unwrap();
+                request.split("\r\n").for_each(|req| {
+                    match handle_request(&req, &mut _stream) {
+                        Ok(_) => (),
+                        Err(e) => println!("error: {}", e),
+                    }
+                });
             }
             Err(e) => {
                 println!("error: {}", e);
