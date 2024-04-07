f1340e5b6d3d5285e9dde8b1ee7a562b24373c00
Oskar Wiszowaty <ppwiszu@gmail.com>
2024-04-07 21:41:14 +0200

    Ping

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,39 @@
-use std::net::TcpListener;
+use std::{
+    io::{Read, Write},
+    net::{TcpListener, TcpStream},
+};
+
+fn handle_request(request: &str, stream: &mut TcpStream) -> Result<(), String> {
+    match request {
+        "PING" => {
+            let response = b"+PONG\r\n";
+            match stream.write_all(response) {
+                Ok(_) => Ok(()),
+                Err(e) => Err(e.to_string()),
+            }
+        }
+        _ => Err(format!("Do not support request {}", request)),
+    }
+}
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    
+
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut _stream) => {
                 println!("accepted new connection");
+                let mut request = String::new();
+                _stream.read_to_string(&mut request).unwrap();
+
+                match handle_request(&request, &mut _stream) {
+                    Ok(_) => (),
+                    Err(e) => println!("error: {}", e)
+                }
+
             }
             Err(e) => {
                 println!("error: {}", e);
